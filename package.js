56acf1c3216ac43d5c6011ed1bcf4cab7f81009d
Jimmie Butler <jimmiebtlr@gmail.com>
2015-11-03 20:48:31 -0600

    Bump versions.

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 Package.describe({
-    name: "elevatedevdesign:cart-bootstrap",
-    summary: "Bootstrap templates for use with elevatedevdesign:cart.",
+    name: "jimmiebtlr:cart-bootstrap",
+    summary: "Bootstrap templates for use with jimmiebtlr:cart.",
   	version: "0.0.1-rc.2",
-    git: "https://github.com/ElevateDev/meteor-cart-bootstrap.git"
+    git: "https://github.com/jimmiebtlr/meteor-cart-bootstrap.git"
 });
 
 Package.on_use(function (api) {
 	api.versionsFrom("METEOR@1.0.2");
 
   api.use([
-    "elevatedevdesign:cart@0.0.1-rc.2",
+    "jimmiebtlr:cart@0.0.2-rc.1",
     'lepozepo:accounting@1.0.0',
     'templating',
     'ui',
     'reactive-var',
     'deps@1.0.7'
   ]);
-	
+
 	api.add_files([
     'templates.html',
     'templates.js',
